40be970cf30acbe4a263eab3efbf5de6a0651478
Mihailo Milenkovic <mmilenkovic@groq.com>
2024-06-16 03:39:07 -0700

    use all config params

--- src/dataset.py
+++ src/dataset.py
@@ -6,19 +6,21 @@ from PIL import Image
 from torch.utils.data import Dataset
 import torchvision.transforms as transforms
 
+
 class FacesDataSet(Dataset):
-    def __init__(self, img_size, image_dir:str, crop_size=128, total_images=None):
+    def __init__(self, img_size, image_dir: str, crop_size=128, total_images=None):
         assert img_size <= crop_size <= 250
         self.img_size = img_size
         self.crop_size = crop_size
         self.data_root = os.path.abspath(image_dir)
-        self.image_paths = glob.glob(os.path.join(self.data_root, "*.jpg"))
+        self.image_paths = glob.glob(os.path.join(self.data_root, "*.png"))
         self.image_paths = self.image_paths[:total_images]
         self.transforms = transforms.Compose(
             [
                 # transforms.CenterCrop((self.crop_size, self.crop_size)),
+                transforms.Grayscale(),  # TODO: make sure we set the channels based on the training config, currently hardcoding grayscale
                 transforms.Resize((self.crop_size, self.crop_size)),
-                transforms.ToTensor() # this puts the image in the CxHxW format and normalizes it to [0,1). See if we want this!
+                transforms.ToTensor(),  # this puts the image in the CxHxW format and normalizes it to [0,1). See if we want this!
             ]
         )
 
@@ -26,6 +28,6 @@ class FacesDataSet(Dataset):
         return len(self.image_paths)
 
     def __getitem__(self, index):
-        image_path =  self.image_paths[index]
+        image_path = self.image_paths[index]
         image = Image.open(image_path)
         return self.transforms(image)
--- src/training.py
+++ src/training.py
@@ -13,22 +13,10 @@ from critics import FCCritic
 from generators import FCGenerator
 from dataset import FacesDataSet
 
-IMG_SIZE = 128  # Images will be IMG_SIZExIMG_SIZE
-CHANNELS = 1  # Grayscale
-Z_SIZE = 100  # Size of latent vector
-
 DEVICE = torch.device(
     "cuda" if torch.cuda.is_available() else "cpu"
 )  # Boilerplate code for using CUDA for faster training
 CUDA = torch.cuda.is_available()  # Use CUDA for faster training
-MAX_SUMMARY_IMAGES = 4  # How many images to output to Tensorboard
-LR = 1e-4  # Learning rate
-EPOCHS = 100  # Number of epochs
-BATCH_SIZE = 64  # Number of images in a batch
-NUM_WORKERS = (
-    8  # How many parallel workers for data ingestion. NOTE: Set to 0 when debugging
-)
-CLIP_VALUE = 1e-2  # Used to clip the parameters of the critic network
 
 
 # TODO: use all values here
@@ -37,6 +25,7 @@ class TrainingConfig:
     dataset_location: str
     training_images_to_use: int
     batch_size: int
+    max_summary_images: int
     image_size: int
     channels: int
     z_size: int
@@ -47,16 +36,16 @@ class TrainingConfig:
     wandb_relogin: bool
     wandb_api_key: str
 
+    def __post_init__(self):
+        assert (
+            self.max_summary_images <= self.batch_size
+        ), "We can only write to Tensorboard as many images as there are in a batch"
+
     @classmethod
     def from_yaml(cls, yaml_file: str) -> "TrainingConfig":
         with open(yaml_file, "r") as f:
             yaml_data = yaml.safe_load(f)
-        return cls(yaml_data)
-
-
-assert (
-    MAX_SUMMARY_IMAGES <= BATCH_SIZE
-), "We can only write to Tensorboard as many images as there are in a batch"
+        return cls(**yaml_data)
 
 
 def train(training_config: TrainingConfig):
@@ -64,32 +53,34 @@ def train(training_config: TrainingConfig):
     # summary_writer = SummaryWriter()
 
     # Initialize the critic and the generator. NOTE: You can use other classes from critis.py and generators.py here.
-    critic = FCCritic(IMG_SIZE, CHANNELS)
-    generator = FCGenerator(IMG_SIZE, CHANNELS, Z_SIZE)
+    critic = FCCritic(training_config.image_size, training_config.channels)
+    generator = FCGenerator(
+        training_config.image_size, training_config.channels, training_config.z_size
+    )
 
     critic.to(DEVICE)
     generator.to(DEVICE)
 
     # Initialize the data set. NOTE: You can pass total_images argument to avoid loading the whole dataset.
     data_set = FacesDataSet(
-        IMG_SIZE,
+        training_config.image_size,
         total_images=training_config.training_images_to_use,
         image_dir=training_config.dataset_location,
     )
-    total_iterations = len(data_set) // BATCH_SIZE
+    total_iterations = len(data_set) // training_config.batch_size
     data_loader = DataLoader(
         data_set,
-        batch_size=BATCH_SIZE,
+        batch_size=training_config.batch_size,
         shuffle=True,
         pin_memory=True,
-        num_workers=NUM_WORKERS,
+        num_workers=training_config.num_workers,
         drop_last=True,
     )
 
-    optimizer_c = torch.optim.RMSprop(critic.parameters(), lr=LR)
-    optimizer_g = torch.optim.RMSprop(generator.parameters(), lr=LR)
+    optimizer_c = torch.optim.RMSprop(critic.parameters(), lr=training_config.lr)
+    optimizer_g = torch.optim.RMSprop(generator.parameters(), lr=training_config.lr)
 
-    for epoch in range(EPOCHS):
+    for epoch in range(training_config.epochs):
         if epoch < 10:
             c_times = 100
         else:
@@ -103,7 +94,7 @@ def train(training_config: TrainingConfig):
         ):
             # Global step is used in tensorboard so that we have unique number for each batch of data
             global_step = epoch * total_iterations + i
-            z_batch = torch.randn((BATCH_SIZE, Z_SIZE))
+            z_batch = torch.randn((training_config.batch_size, training_config.z_size))
             # model = FCGenerator(IMG_SIZE, CHANNELS, Z_SIZE)
 
             # This is just boilerplate if you're using CUDA - All inputs to the network need to be on the same device
@@ -126,7 +117,7 @@ def train(training_config: TrainingConfig):
             optimizer_c.step()
             # Clip the weights of the critic to satisfy the Lipschitz constraint
             for p in critic.parameters():
-                p.data.clamp_(-CLIP_VALUE, CLIP_VALUE)
+                p.data.clamp_(-training_config.clip_value, training_config.clip_value)
 
             # Train the generator only after the critic has been trained c_times
             if i % c_times == 0:
@@ -145,7 +136,8 @@ def train(training_config: TrainingConfig):
 
                 # summary_writer.add_images("Generated images", gen_imgs[:MAX_SUMMARY_IMAGES], global_step)
                 images = wandb.Image(
-                    gen_imgs[:MAX_SUMMARY_IMAGES], caption="Top: Output, Bottom: Input"
+                    gen_imgs[: training_config.max_summary_images],
+                    caption="Top: Output, Bottom: Input",
                 )
                 wandb.log({"Generated images": images}, step=global_step)
 
